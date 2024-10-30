faaa8644b0b055a5edc80b307cb88b190b13bea5
taoitw <kshih200@gmail.com>
2024-10-30 00:16:13 -0400

    Change build output directory to docs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './', // Set base path to relative
-  plugins: [react()],
+  build: {
+    outDir: 'docs', // Change the output directory to docs/
+  },
 });
+
 
 
